07bb5d080351a40278aa8b38b93562e419c71eb2
Lynn Myat <lynnmyat.tk@gmail.com>
2026-04-05 22:25:14 +0300

    feat: initial project setup

---
+++ src/main/java/com/tk/login_with_user_from_db/LoginWithUserFromDbApplication.java
@@ -0,0 +1,13 @@
+package com.tk.login_with_user_from_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithUserFromDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithUserFromDbApplication.class, args);
+	}
+
+}
